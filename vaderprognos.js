e5431a131ad0fae33df6e33c9fb4ced8f44e8e90
timothylevin <167991082+timothylevin@users.noreply.github.com>
2024-05-24 10:56:26 +0200

    Update vaderprognos.js

--- vaderprognos.js
+++ vaderprognos.js
@@ -1,50 +1,3 @@
-const translateWeatherSymbol = (symbolCode) => {
-    switch(symbolCode) {
-        case 'clearsky_day':
-            return 'klart väder (dag)';
-        case 'clearsky_night':
-            return 'klart väder (natt)';
-        case 'partlycloudy_day':
-            return 'delvis molnigt (dag)';
-        case 'partlycloudy_night':
-            return 'delvis molnigt (natt)';
-        case 'cloudy':
-            return 'molnigt';
-        case 'fair_day':
-            return 'växlande molnighet (dag)';
-        case 'fair_night':
-            return 'växlande molnighet (natt)';
-        case 'rain':
-            return 'regn';
-        case 'lightrain_showers_day':
-            return 'lätt regnskurar (dag)';
-        case 'lightrain_showers_night':
-            return 'lätt regnskurar (natt)';
-        case 'rain_showers_day':
-            return 'regnskurar (dag)';
-        case 'rain_showers_night':
-            return 'regnskurar (natt)';
-        case 'heavyrain':
-            return 'kraftigt regn';
-        case 'thunderstorm':
-            return 'åska';
-        case 'sleet':
-            return 'snöblandat regn';
-        case 'snow':
-            return 'snö';
-        case 'sleet_showers_day':
-            return 'snöblandade regnskurar (dag)';
-        case 'sleet_showers_night':
-            return 'snöblandade regnskurar (natt)';
-        case 'snow_showers_day':
-            return 'snöskurar (dag)';
-        case 'snow_showers_night':
-            return 'snöskurar (natt)';
-        default:
-            return `okänt väder (${symbolCode})`;
-    }
-}
-// Funktion för att hämta väderprognosen från en väder-API baserat på givna latitud- och longitudvärden.
 const getWeatherForecast = (latitude, longitude) => {
     const forecastAPIURL = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longitude}`;
 
@@ -85,7 +38,3 @@ const getWeatherForecast = (latitude, longitude) => {
         });
 }
 
-// Hjälpfunktion för att formatera tiden i ett läsbart format.
-const formatTime = (time) => {
-    return time.toLocaleTimeString('sv-SE', { hour: '2-digit', minute: '2-digit' });
-}
