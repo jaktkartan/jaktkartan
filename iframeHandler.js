e5fd2029415b26488a09a108c1f5b92953644e2b
timothylevin <167991082+timothylevin@users.noreply.github.com>
2024-05-30 11:23:27 +0200

    Update iframeHandler.js

--- iframeHandler.js
+++ iframeHandler.js
@@ -4,6 +4,8 @@ function showIframeAndHideButtons(url, county) {
     var iframeContainer = document.getElementById('iframe-container');
     var countyButtons = document.getElementById('county-buttons');
     var backButton = document.getElementById('back-button');
+    var mainTitle = document.getElementById('main-title');
+    var mainInfo = document.getElementById('main-info');
 
     currentCounty = county; // Sätt det aktuella länet
 
@@ -14,7 +16,8 @@ function showIframeAndHideButtons(url, county) {
     iframeContainer.innerHTML = '';
     iframeContainer.appendChild(iframe);
     countyButtons.style.display = 'none'; // Dölj knapparna när iframen visas
-    iframeContainer.style.display = 'block';
+    mainTitle.classList.add('hide-on-selection'); // Dölj rubriken när iframen visas
+    mainInfo.classList.add('hide-on-selection'); // Dölj informationsdelen när iframen visas
     backButton.classList.remove('hidden'); // Visa tillbaka knappen
 }
 
@@ -22,10 +25,14 @@ function goBack() {
     var iframeContainer = document.getElementById('iframe-container');
     var countyButtons = document.getElementById('county-buttons');
     var backButton = document.getElementById('back-button');
+    var mainTitle = document.getElementById('main-title');
+    var mainInfo = document.getElementById('main-info');
 
     currentCounty = ""; // Nollställ det aktuella länet
 
     iframeContainer.innerHTML = ''; // Ta bort iframen
     countyButtons.style.display = 'block'; // Visa knapparna igen
+    mainTitle.classList.remove('hide-on-selection'); // Visa rubriken igen
+    mainInfo.classList.remove('hide-on-selection'); // Visa informationsdelen igen
     backButton.classList.add('hidden'); // Dölj tillbaka knappen
 }
