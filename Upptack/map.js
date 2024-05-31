146212ed56ed34014f29346caefe7dc66005d148
timothylevin <167991082+timothylevin@users.noreply.github.com>
2024-05-31 20:51:41 +0200

    Update map.js

--- Upptack/map.js
+++ Upptack/map.js
@@ -1,12 +1,8 @@
 // Upptack/map.js
 
 document.addEventListener("DOMContentLoaded", function() {
-    var map = L.map('map').setView([62.0, 15.0], 5);
+    var map = L.map('map');
     var geojsonData;
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
 
     // Load the GeoJSON data
     axios.get('../Upptack/geojson/handelser.geojson')
