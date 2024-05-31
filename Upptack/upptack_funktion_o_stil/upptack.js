76e148c4d04d174f1cc37c990ff0f9021425ea71
timothylevin <167991082+timothylevin@users.noreply.github.com>
2024-06-01 00:07:50 +0200

    Update upptack.js

--- Upptack/upptack_funktion_o_stil/upptack.js
+++ Upptack/upptack_funktion_o_stil/upptack.js
@@ -4,10 +4,12 @@ axios.get('Upptack/upptack.geojson')
         console.log("Successfully fetched GeoJSON data:", response.data);
         var geojsonLayer = L.geoJSON(response.data, {
             pointToLayer: function (feature, latlng) {
+                console.log("Creating point layer for feature:", feature);
                 // Här kan du anpassa punkternas utseende baserat på egenskaperna (feature.properties)
                 return L.marker(latlng);
             },
             onEachFeature: function (feature, layer) {
+                console.log("Adding popup for feature:", feature);
                 // Här kan du lägga till popup-information för varje punkt
                 var popupContent = "<b>" + feature.properties.NAMN + "</b><br>" +
                                     "Typ: " + feature.properties.TYP + "<br>" +
@@ -18,7 +20,9 @@ axios.get('Upptack/upptack.geojson')
 
         // Hämta alla standardmarkörer och ändra deras display-attribut till block för att göra dem synliga
         var standardMarkers = document.querySelectorAll('.leaflet-marker-icon[src*="marker-icon.png"]');
+        console.log("Found standard markers:", standardMarkers);
         standardMarkers.forEach(function(marker) {
+            console.log("Changing display attribute for marker:", marker);
             marker.style.display = 'block';
         });
     })
