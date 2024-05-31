2d2ef781de903cb3ff973173b22bb5c8f0c11062
timothylevin <167991082+timothylevin@users.noreply.github.com>
2024-06-01 01:41:53 +0200

    Delete Upptack/upptack_funktion_o_stil directory

--- Upptack/upptack_funktion_o_stil/upptack.js
+++
@@ -1,8 +0,0 @@
-axios.get('Upptack/upptack.geojson')
-    .then(function (response) {
-        console.log("Successfully fetched GeoJSON data:", response.data);
-        var geojsonLayer = L.geoJSON(response.data).addTo(map);
-    })
-    .catch(function (error) {
-        console.log("Error fetching GeoJSON data:", error.message);
-    });
