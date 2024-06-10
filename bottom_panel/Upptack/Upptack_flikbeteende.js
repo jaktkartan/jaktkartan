4b42ebbbca51ef658528c40d2b9256ffa5b5a50a
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-10 13:42:14 +0200

    Update Upptack_flikbeteende.js

--- bottom_panel/Upptack/Upptack_flikbeteende.js
+++ bottom_panel/Upptack/Upptack_flikbeteende.js
@@ -1,12 +1,1 @@
-function tab1SpecificLogic() {
-    // Specifik logik för Upptäck-fliken
-    console.log("Running specific logic for Upptäck tab");
 
-    var upptackTabContent = document.querySelector("#tab1");
-
-    // Tillämpa CSS-klassen för höjd och andra egenskaper
-    upptackTabContent.classList.add("tab1");
-
-    // Exempel på ytterligare logik för Upptäck-fliken
-    // ...
-}
