fe9233efce76356d0127ff5aab92a2c6c5e3649a
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-11 10:55:17 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -3,15 +3,13 @@
 function centerPopup(e) {
     var map = e.target._map; // Hämta kartan från det objekt som öppnade popupen
     
-    // Kontrollera om e.popup är definierad och e.popup._latlng är definierat innan du fortsätter
-    if (e.popup && e.popup._latlng) {
+    // Kontrollera om e.popup är definierad innan du fortsätter
+    if (e.popup) {
         var px = map.project(e.popup._latlng); // Hitta popupens position
         px.y -= map.getSize().y / 2; // Justera för att centrera popup
         map.panTo(map.unproject(px), { animate: true }); // Panorera kartan
     }
 }
-
-
 
 function addGlobalPopupHandler(map) {
     map.on('popupopen', centerPopup);
