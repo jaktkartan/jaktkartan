14928d60eb3bbf867dc14c5805e55986243e6b53
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-11 11:23:10 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -1,13 +1,1 @@
-// Anta att "map" är en referens till din Leaflet-karta
 
-// Lägg till en händelselyssnare för klick på markörer
-map.on('click', function(event) {
-    // Kontrollera om klicket inträffade på en markör
-    if (event.layer instanceof L.Marker) {
-        // Hämta den geografiska positionen för markören
-        var markerLatLng = event.latlng;
-
-        // Panorera kartan så att markören hamnar i mitten
-        map.panTo(markerLatLng, { animate: true });
-    }
-});
