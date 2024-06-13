ab731bf685f539808d83cdeec3702df7a511f0a5
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-13 19:42:38 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -1,25 +1,37 @@
-// Funktion för att skapa en marker med popup-fönster för en bild-URL från GeoJSON
+// Funktion för att skapa en marker med popup-fönster för bilder från GeoJSON
 function createMarkerWithPopup(map, feature) {
     var properties = feature.properties;
-    var imageUrl = properties.bild_baver; // Anpassa detta beroende på din GeoJSON-struktur
+    var imageUrls = findImageUrls(properties);
 
-    if (imageUrl) {
+    if (imageUrls.length > 0) {
         var marker = L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]).addTo(map);
-        
-        // Ladda bilden och skapa popup-fönster
-        axios.get(imageUrl)
-            .then(function (response) {
-                var imageContent = `<img src="${imageUrl}" alt="Image">`;
-                var popup = L.popup().setContent(imageContent);
-                marker.bindPopup(popup);
-            })
-            .catch(function (error) {
-                console.error('Failed to fetch image:', error);
-                var errorMessage = "Failed to load image";
-                var popup = L.popup().setContent(errorMessage);
-                marker.bindPopup(popup);
-            });
+
+        // Ladda bilderna och skapa popup-fönster
+        var popupContent = imageUrls.map(function(imageUrl) {
+            return `<img src="${imageUrl}" alt="Image">`;
+        }).join('<br>'); // Använd '<br>' för att separera bilder vertikalt
+
+        var popup = L.popup().setContent(popupContent);
+        marker.bindPopup(popup);
     }
+}
+
+// Funktion för att hitta alla egenskaper med giltiga bild-URL:er från GeoJSON
+function findImageUrls(properties) {
+    var imageUrls = [];
+
+    for (var key in properties) {
+        if (properties.hasOwnProperty(key) && typeof properties[key] === 'string' && isImageUrl(properties[key])) {
+            imageUrls.push(properties[key]);
+        }
+    }
+
+    return imageUrls;
+}
+
+// Funktion för att avgöra om en given sträng är en giltig bild-URL
+function isImageUrl(url) {
+    return (url.match(/\.(jpeg|jpg|gif|png|webp)$/) != null);
 }
 
 // Exempel på hur du kan använda funktionen med din GeoJSON-data
