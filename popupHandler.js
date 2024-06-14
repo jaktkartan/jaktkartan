4b888a6de59d10cba608c165458e2e404f276d2a
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-14 23:28:41 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -12,13 +12,29 @@ popupPanel.style.borderRight = '5px solid rgb(50, 94, 88)';
 popupPanel.style.padding = '10px';
 popupPanel.style.boxShadow = '0 0 10px rgba(0,0,0,0.1)';
 popupPanel.style.zIndex = '1000';
-popupPanel.style.display = 'none';
 popupPanel.style.overflowY = 'auto'; // Lägger till scrollbar vid behov
 popupPanel.style.wordWrap = 'break-word'; // Bryter text vid behov
 popupPanel.style.borderTopLeftRadius = '10px'; // Rundar övre vänstra hörnet
 popupPanel.style.borderTopRightRadius = '10px'; // Rundar övre högra hörnet
 popupPanel.style.fontFamily = "'Roboto', sans-serif"; // Använder Roboto-typsnittet
 popupPanel.style.color = 'rgb(50, 94, 88)';
+popupPanel.style.transform = 'translateY(100%)'; // Startposition för transition
+
+// Funktion för att uppdatera panelinnehållet baserat på egenskaper från geojson-objekt
+function updatePopupPanelContent(properties) {
+    var panelContent = document.getElementById('popup-panel-content');
+    if (!panelContent) {
+        console.error("Elementet 'popup-panel-content' hittades inte.");
+        return;
+    }
+
+    var content = '';
+    for (var key in properties) {
+        if (properties.hasOwnProperty(key)) {
+            var value = properties[key];
+            content += '<p><strong>' + key + ':</strong> ' + value + '</p>';
+        }
+    }
 
 
 // Funktion för att uppdatera panelinnehållet baserat på egenskaper från geojson-objekt
