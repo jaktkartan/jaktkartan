2c384865486f93a39d540a3a4f939cb06a60da19
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-16 20:45:58 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -18,8 +18,7 @@ popupPanel.style.borderTopLeftRadius = '10px';
 popupPanel.style.borderTopRightRadius = '10px';
 popupPanel.style.fontFamily = "'Roboto', sans-serif";
 popupPanel.style.color = 'rgb(50, 94, 88)';
-popupPanel.style.transform = 'translateY(100%)';
-popupPanel.style.transition = 'transform 0.3s ease';
+popupPanel.style.display = 'none'; // Göm panelen som standard
 
 // Håll koll på om popup-panelen är synlig eller inte
 var popupPanelVisible = false;
@@ -30,19 +29,13 @@ function showPopupPanel(properties) {
 
     // Visa popup-panelen
     popupPanel.style.display = 'block';
-    setTimeout(function() {
-        popupPanel.style.transform = 'translateY(0%)';
-    }, 10);
     popupPanelVisible = true;
 }
 
 // Funktion för att dölja popup-panelen
 function hidePopupPanel() {
-    // Dölj popup-panelen med en animation
-    popupPanel.style.transform = 'translateY(100%)';
-    setTimeout(function() {
-        popupPanel.style.display = 'none';
-    }, 300);
+    // Dölj popup-panelen
+    popupPanel.style.display = 'none';
     popupPanelVisible = false;
 }
 
@@ -85,9 +78,6 @@ function addClickHandlerToLayer(layer) {
                 } else {
                     updatePopupPanelContent(properties);
                 }
-
-                // Stoppa bubbla av klickhändelse för att förhindra att document-click listenern aktiveras
-                shouldClosePopupPanel = false;
             } else {
                 console.error('Ingen geojson-information hittades i klickhändelsen.');
             }
@@ -99,14 +89,15 @@ function addClickHandlerToLayer(layer) {
 
 // Eventlyssnare för att stänga popup-panelen vid klick utanför
 document.addEventListener('click', function(event) {
-    if (popupPanelVisible && !popupPanel.contains(event.target) && !event.target.closest('.leaflet-popup')) {
+    if (popupPanelVisible && !popupPanel.contains(event.target)) {
         hidePopupPanel();
     }
-}, true);
+});
 
 // Lägg till en eventlyssnare för kartans moveend-händelse för att tillåta interaktion med objekt under panorering
 map.on('moveend', function() {
-    shouldClosePopupPanel = true;
+    // Stäng popup-panelen när kartan panoreras
+    hidePopupPanel();
 });
 
 // Kontrollera att popup-panelen finns och har nödvändiga HTML-element
