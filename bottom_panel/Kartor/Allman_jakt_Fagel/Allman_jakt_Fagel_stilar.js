752ebfb42ae0710c68b4de61fe1cf74c4460b96e
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-18 04:12:16 +0200

    Update Allman_jakt_Fagel_stilar.js

--- bottom_panel/Kartor/Allman_jakt_Fagel/Allman_jakt_Fagel_stilar.js
+++ bottom_panel/Kartor/Allman_jakt_Fagel/Allman_jakt_Fagel_stilar.js
@@ -1,1 +1,38 @@
 
+// Allman_jakt_Fagel_stilar.js
+
+// Standardstil för Allmän jakt: Fågel
+const defaultStyle = {
+    color: "green",
+    weight: 2,
+    opacity: 1
+};
+
+// Highlight-stil för Allmän jakt: Fågel (vid hover)
+const highlightStyle = {
+    color: "yellow",
+    weight: 3,
+    opacity: 1
+};
+
+// Funktion som returnerar standardstilen
+function getDefaultStyle(feature) {
+    return defaultStyle;
+}
+
+// Funktion för att tillämpa stilar och hantera interaktioner
+function applyAllmanJaktFagelStyle(feature, layer) {
+    layer.setStyle(getDefaultStyle(feature));
+
+    layer.on({
+        mouseover: function() {
+            layer.setStyle(highlightStyle);
+        },
+        mouseout: function() {
+            layer.setStyle(getDefaultStyle(feature));
+        }
+    });
+}
+
+// Exportera stilfunktionerna
+export { getDefaultStyle, applyAllmanJaktFagelStyle };
