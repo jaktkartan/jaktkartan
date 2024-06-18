6d69e3035881bcd4d649900db4fed36893eb720f
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-18 04:25:39 +0200

    Update Algjaktskartan_stilar.js

--- bottom_panel/Kartor/Algjaktskartan/Algjaktskartan_stilar.js
+++ bottom_panel/Kartor/Algjaktskartan/Algjaktskartan_stilar.js
@@ -1,25 +1,21 @@
 // Algjaktskartan_stilar.js
 
-// Standardstil för Algjaktskartan
 const defaultStyle = {
     color: "blue",
     weight: 2,
     opacity: 1
 };
 
-// Highlight-stil för Algjaktskartan (vid hover)
 const highlightStyle = {
     color: "yellow",
     weight: 3,
     opacity: 1
 };
 
-// Funktion som returnerar standardstilen
 function getDefaultStyle(feature) {
     return defaultStyle;
 }
 
-// Funktion för att tillämpa stilar och hantera interaktioner
 function applyAlgjaktskartanStyle(feature, layer) {
     layer.setStyle(getDefaultStyle(feature));
 
@@ -33,5 +29,4 @@ function applyAlgjaktskartanStyle(feature, layer) {
     });
 }
 
-// Exportera stilfunktionerna
 export { getDefaultStyle, applyAlgjaktskartanStyle };
