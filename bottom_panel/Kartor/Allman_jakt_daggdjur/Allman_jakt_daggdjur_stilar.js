9507322c23d6b80e436cb8573e90d3aeee0c9f26
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-18 04:12:43 +0200

    Update Allman_jakt_daggdjur_stilar.js

--- bottom_panel/Kartor/Allman_jakt_daggdjur/Allman_jakt_daggdjur_stilar.js
+++ bottom_panel/Kartor/Allman_jakt_daggdjur/Allman_jakt_daggdjur_stilar.js
@@ -1,1 +1,37 @@
+// Allman_jakt_daggdjur_stilar.js
 
+// Standardstil för Allmän jakt: Däggdjur
+const defaultStyle = {
+    color: "red",
+    weight: 2,
+    opacity: 1
+};
+
+// Highlight-stil för Allmän jakt: Däggdjur (vid hover)
+const highlightStyle = {
+    color: "yellow",
+    weight: 3,
+    opacity: 1
+};
+
+// Funktion som returnerar standardstilen
+function getDefaultStyle(feature) {
+    return defaultStyle;
+}
+
+// Funktion för att tillämpa stilar och hantera interaktioner
+function applyAllmanJaktDaggdjurStyle(feature, layer) {
+    layer.setStyle(getDefaultStyle(feature));
+
+    layer.on({
+        mouseover: function() {
+            layer.setStyle(highlightStyle);
+        },
+        mouseout: function() {
+            layer.setStyle(getDefaultStyle(feature));
+        }
+    });
+}
+
+// Exportera stilfunktionerna
+export { getDefaultStyle, applyAllmanJaktDaggdjurStyle };
