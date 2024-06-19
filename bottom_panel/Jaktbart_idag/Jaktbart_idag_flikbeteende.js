027faa0f1b9e51af07414c7dbeaf06a3955c023e
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-19 22:27:16 +0200

    Update Jaktbart_idag_flikbeteende.js

--- bottom_panel/Jaktbart_idag/Jaktbart_idag_flikbeteende.js
+++ bottom_panel/Jaktbart_idag/Jaktbart_idag_flikbeteende.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Hämta latitud och longitud från localStorage
-    var lat = parseFloat(localStorage.getItem('userLatitude'));
-    var lon = parseFloat(localStorage.getItem('userLongitude'));
-
-    // Visa latitud och longitud på sidan
     var latitudeElement = document.getElementById('latitude');
     var longitudeElement = document.getElementById('longitude');
 
-    if (lat && lon) {
-        latitudeElement.textContent = lat.toFixed(6); // Justera precisionen efter behov
-        longitudeElement.textContent = lon.toFixed(6); // Justera precisionen efter behov
-    } else {
-        latitudeElement.textContent = 'N/A';
-        longitudeElement.textContent = 'N/A';
-    }
+    if (latitudeElement && longitudeElement) {
+        // Hämta latitud och longitud från localStorage
+        var lat = parseFloat(localStorage.getItem('userLatitude'));
+        var lon = parseFloat(localStorage.getItem('userLongitude'));
 
-    // Gör något mer med lat och lon här om det behövs
+        // Visa latitud och longitud på sidan
+        if (lat && lon) {
+            latitudeElement.textContent = lat.toFixed(6); // Justera precisionen efter behov
+            longitudeElement.textContent = lon.toFixed(6); // Justera precisionen efter behov
+        } else {
+            latitudeElement.textContent = 'N/A';
+            longitudeElement.textContent = 'N/A';
+        }
+    } else {
+        console.error('Kunde inte hitta latitude eller longitude element.');
+    }
 });
