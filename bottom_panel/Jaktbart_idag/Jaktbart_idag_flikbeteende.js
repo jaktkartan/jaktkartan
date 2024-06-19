b653adec27b0b307694863094ddc2ad9fdab07f2
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-19 22:53:54 +0200

    Update Jaktbart_idag_flikbeteende.js

--- bottom_panel/Jaktbart_idag/Jaktbart_idag_flikbeteende.js
+++ bottom_panel/Jaktbart_idag/Jaktbart_idag_flikbeteende.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Logga hela innehållet i localStorage
-    console.log('localStorage innehåll:', JSON.stringify(localStorage));
+    function updatePosition() {
+        var latitudeElement = document.getElementById('latitude');
+        var longitudeElement = document.getElementById('longitude');
 
-    var latitudeElement = document.getElementById('latitude');
-    var longitudeElement = document.getElementById('longitude');
+        if (latitudeElement && longitudeElement) {
+            // Hämta latitud och longitud från localStorage
+            var lat = parseFloat(localStorage.getItem('userLatitude'));
+            var lon = parseFloat(localStorage.getItem('userLongitude'));
 
-    if (latitudeElement && longitudeElement) {
-        // Hämta latitud och longitud från localStorage
-        var lat = parseFloat(localStorage.getItem('userLatitude'));
-        var lon = parseFloat(localStorage.getItem('userLongitude'));
-
-        // Logga latitud och longitud
-        console.log('Latitude från localStorage:', lat);
-        console.log('Longitude från localStorage:', lon);
-
-        // Visa latitud och longitud på sidan
-        if (!isNaN(lat) && !isNaN(lon)) {
-            latitudeElement.textContent = lat.toFixed(6); // Justera precisionen efter behov
-            longitudeElement.textContent = lon.toFixed(6); // Justera precisionen efter behov
+            // Visa latitud och longitud på sidan
+            if (lat && lon) {
+                latitudeElement.textContent = lat.toFixed(6); // Justera precisionen efter behov
+                longitudeElement.textContent = lon.toFixed(6); // Justera precisionen efter behov
+            } else {
+                latitudeElement.textContent = 'N/A';
+                longitudeElement.textContent = 'N/A';
+            }
         } else {
-            latitudeElement.textContent = 'N/A';
-            longitudeElement.textContent = 'N/A';
+            console.error('Kunde inte hitta latitude eller longitude element.');
         }
-    } else {
-        console.error('Kunde inte hitta latitude eller longitude element.');
     }
+
+    // Kör updatePosition när sidan laddas
+    updatePosition();
+
+    // Kör updatePosition när localStorage uppdateras
+    window.addEventListener('storage', updatePosition);
 });
