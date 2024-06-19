f5aff167053b338eddeb7f44ab6db2ba96b9a0b2
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-19 21:42:16 +0200

    Update tabHandler.js

--- tabHandler.js
+++ tabHandler.js
@@ -100,7 +100,7 @@ function openTabWithCoordinates(tabId, url) {
     var queryString = `?lat=${window.userLatitude}&lon=${window.userLongitude}`;
     var fullUrl = url + queryString;
 
-    console.log('Full URL:', fullUrl); // Logga den fullständiga URL:en för att kontrollera
+    console.log('Full URL:', fullUrl); // Lägg till denna logg för att verifiera den fullständiga URL:en
 
     resetTabs(); // Återställ flikarna innan en ny öppnas
     var tab = document.getElementById(tabId);
