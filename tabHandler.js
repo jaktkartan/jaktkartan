57d0205596001cfba21188119e71ed6f1acb7ad2
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-20 11:00:59 +0200

    Update tabHandler.js

--- tabHandler.js
+++ tabHandler.js
@@ -19,17 +19,6 @@ function togglePanel() {
     }
 }
 
-// Funktion för att återställa flikarna till sitt ursprungliga tillstånd
-function resetTabs() {
-    var tabs = document.getElementsByClassName('tab-pane');
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].style.display = 'none'; // Göm flikarna
-        tabs[i].innerHTML = ''; // Ta bort innehållet i flikarna
-    }
-    var tabContent = document.getElementById('tab-content');
-    tabContent.style.display = 'none'; // Göm flikinnehållet
-}
-
 // Funktion för att öppna en flik
 function openTab(tabId, url) {
     resetTabs(); // Återställ flikarna innan en ny öppnas
@@ -135,6 +124,17 @@ function openKaliberkravTab(url) {
         .catch(error => {
             console.error('Error fetching Kaliberkrav content:', error);
         });
+}
+
+// Funktion för att återställa flikarna till sitt ursprungliga tillstånd
+function resetTabs() {
+    var tabs = document.getElementsByClassName('tab-pane');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none'; // Göm flikarna
+        tabs[i].innerHTML = ''; // Ta bort innehållet i flikarna
+    }
+    var tabContent = document.getElementById('tab-content');
+    tabContent.style.display = 'none'; // Göm flikinnehållet
 }
 
 // Händelselyssnare för att hantera klick utanför flikarna och panelknapparna
