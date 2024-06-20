1d9150278d0f29f3f60b32d2d92851ca46566046
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-20 09:37:32 +0200

    Update Jaktbart_idag_flikbeteende.js

--- bottom_panel/Jaktbart_idag/Jaktbart_idag_flikbeteende.js
+++ bottom_panel/Jaktbart_idag/Jaktbart_idag_flikbeteende.js
@@ -1,11 +1,1 @@
-// Jaktbart_idag_flikbeteende.js
-
-function hämtaData() {
-    // Exempel på hämtning av data (ersätt med din faktiska implementation)
-    let data = "Exempel på data från JavaScript-filen.";
-
-    // Uppdatera HTML-elementet med den hämtade datan
-    let dataContainer = document.getElementById('data-container');
-    dataContainer.textContent = data;
-}
 
