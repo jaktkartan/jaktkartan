71061b4aebbd48f522de450a7296daca2a86e72d
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-26 12:34:09 +0200

    Update Upptack_geojsonHandler.js

--- bottom_panel/Upptack/Upptack_geojsonHandler.js
+++ bottom_panel/Upptack/Upptack_geojsonHandler.js
@@ -83,55 +83,103 @@ var Upptack_geojsonHandler = (function() {
         layerIsActive[layerName] = true;
     }
 
-    // Funktion för att tända och släcka lagret
-    function toggleLayer(layerName) {
-        // Om lagret inte är aktivt, eller om det är det första trycket på knappen
-        if (!layerIsActive[layerName] || Object.keys(layerIsActive).every(name => name === layerName)) {
-            // Släck alla andra lager
-            deactivateAllLayersExcept(layerName);
+  // Funktion för att tända och släcka lagret
+function toggleLayer(layerName) {
+    // Om lagret inte är aktivt
+    if (!layerIsActive[layerName]) {
+        // Släck alla andra lager
+        deactivateAllLayersExcept(layerName);
 
-            // Hämta och skapa geoJSON-lager för det valda lagret
-            geojsonLayers[layerName].forEach(function(layer) {
-                layer.addTo(map);
+        // Hämta och skapa geoJSON-lager för det valda lagret
+        fetchGeoJSONDataAndCreateLayer(layerName);
+    } else {
+        // Om lagret redan är aktivt, släck det
+        geojsonLayers[layerName].forEach(function(layer) {
+            map.removeLayer(layer);
+        });
+
+        geojsonLayers[layerName] = [];
+        layerIsActive[layerName] = false;
+    }
+}
+
+// Funktion för att hämta GeoJSON-data och skapa lagret med stil
+function fetchGeoJSONDataAndCreateLayer(layerName) {
+    var geojsonURLs = layerURLs[layerName];
+    geojsonURLs.forEach(function(geojsonURL) {
+        axios.get(geojsonURL)
+            .then(function(response) {
+                console.log("Successfully fetched GeoJSON data:", response.data);
+                var geojson = response.data;
+
+                var layer = L.geoJSON(geojson, {
+                    pointToLayer: function(feature, latlng) {
+                        var filename = getFilenameFromURL(geojsonURL);
+                        var style = layerStyles[layerName][filename];
+                        return L.circleMarker(latlng, style);
+                    },
+                    onEachFeature: function(feature, layer) {
+                        var popupContent = '<div style="max-width: 300px; overflow-y: auto;">';
+
+                        var hideProperties = ['id', 'Aktualitet'];
+                        var hideNameOnlyProperties = ['namn', 'bild', 'info', 'link'];
+
+                        for (var prop in feature.properties) {
+                            if (hideProperties.includes(prop)) {
+                                continue;
+                            }
+                            if (prop === 'BILD') {
+                                popupContent += '<p><img src="' + feature.properties[prop] + '" style="max-width: 100%;" alt="Bild"></p>';
+                            } else if (prop === 'LINK' || prop === 'VAGBESKRIV') {
+                                popupContent += '<p><a href="' + feature.properties[prop] + '" target="_blank">Länk</a></p>';
+                            } else if (hideNameOnlyProperties.includes(prop)) {
+                                popupContent += '<p>' + feature.properties[prop] + '</p>';
+                            } else {
+                                popupContent += '<p><strong>' + prop + ':</strong> ' + feature.properties[prop] + '</p>';
+                            }
+                        }
+                        popupContent += '</div>';
+                        layer.bindPopup(popupContent);
+                    }
+                });
+
+                geojsonLayers[layerName].push(layer);
+
+                if (layerIsActive[layerName]) {
+                    layer.addTo(map);
+                }
+                layerIsActive[layerName] = true;
+            })
+            .catch(function(error) {
+                console.log("Error fetching GeoJSON data:", error.message);
             });
+    });
+}
 
-            layerIsActive[layerName] = true;
-        } else {
-            // Om lagret redan är aktivt, släck det
-            geojsonLayers[layerName].forEach(function(layer) {
+// Funktion för att släcka alla lager utom det angivna lagret
+function deactivateAllLayersExcept(layerName) {
+    Object.keys(layerIsActive).forEach(function(name) {
+        if (name !== layerName && layerIsActive[name]) {
+            geojsonLayers[name].forEach(function(layer) {
                 map.removeLayer(layer);
             });
-
-            geojsonLayers[layerName] = [];
-            layerIsActive[layerName] = false;
+            geojsonLayers[name] = [];
+            layerIsActive[name] = false;
         }
-    }
+    });
+}
 
-    // Funktion för att släcka alla lager utom det angivna lagret
-    function deactivateAllLayersExcept(layerName) {
-        Object.keys(layerIsActive).forEach(function(name) {
-            if (name !== layerName && layerIsActive[name]) {
-                geojsonLayers[name].forEach(function(layer) {
-                    map.removeLayer(layer);
-                });
-                geojsonLayers[name] = [];
-                layerIsActive[name] = false;
-            }
-        });
-    }
+function getFilenameFromURL(url) {
+    var pathArray = url.split('/');
+    var filename = pathArray[pathArray.length - 1];
+    return filename;
+}
 
-    function getFilenameFromURL(url) {
-        var pathArray = url.split('/');
-        var filename = pathArray[pathArray.length - 1];
-        return filename;
-    }
+// Initialisera alla lager vid start
+fetchGeoJSONDataAndCreateLayer('Mässor');
+fetchGeoJSONDataAndCreateLayer('Jaktkort');
+fetchGeoJSONDataAndCreateLayer('Jaktskyttebanor');
 
-    // Initialisera alla lager vid start
-    fetchGeoJSONDataAndCreateLayer('Mässor');
-    fetchGeoJSONDataAndCreateLayer('Jaktkort');
-    fetchGeoJSONDataAndCreateLayer('Jaktskyttebanor');
-
-    return {
-        toggleLayer: toggleLayer
-    };
-})();
+return {
+    toggleLayer: toggleLayer
+};
