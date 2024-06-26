2b8e9f474f571bde0528e7a23992a509a4fb7f93
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-26 16:59:29 +0200

    Update Upptack_geojsonHandler.js

--- bottom_panel/Upptack/Upptack_geojsonHandler.js
+++ bottom_panel/Upptack/Upptack_geojsonHandler.js
@@ -132,10 +132,10 @@ var Upptack_geojsonHandler = (function() {
    // Funktion för att hämta stil baserat på zoomnivå
 function getMarkerStyle(layerName, filename) {
     var zoomLevel = map.getZoom();
-    var scaleFactor = 0.9; // Justera faktorn baserat på erfarenhet och experiment
+    var scaleFactor = 4; // Justera faktorn baserat på erfarenhet och experiment
 
     // Justera radien baserat på zoomnivå och scaleFactor
-    var radius = defaultMarkerSize * Math.pow(scaleFactor, zoomLevel - 4);
+    var radius = defaultMarkerSize * Math.pow(scaleFactor, zoomLevel - 3);
 
     // Anpassa andra stilar här om det behövs
     var style = {
