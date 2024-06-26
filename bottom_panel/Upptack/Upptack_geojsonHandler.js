bfbc018d3832d93c3478a79f4dfe522608a84d8b
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-26 15:18:30 +0200

    Update Upptack_geojsonHandler.js

--- bottom_panel/Upptack/Upptack_geojsonHandler.js
+++ bottom_panel/Upptack/Upptack_geojsonHandler.js
@@ -19,13 +19,13 @@ var Upptack_geojsonHandler = (function() {
 
     var layerStyles = {
         'Mässor': {
-            'Massor.geojson': { color: 'orange', radius: 8, fillColor: 'orange', fillOpacity: 0.8 }
+            'Massor.geojson': { color: 'orange', baseRadius: 8, fillColor: 'orange', fillOpacity: 0.8 }
         },
         'Jaktkort': {
-            'jaktkort.geojson': { color: 'blue', radius: 8, fillColor: 'blue', fillOpacity: 0.8 }
+            'jaktkort.geojson': { color: 'blue', baseRadius: 8, fillColor: 'blue', fillOpacity: 0.8 }
         },
         'Jaktskyttebanor': {
-            'jaktskyttebanor.geojson': { color: 'green', radius: 8, fillColor: 'green', fillOpacity: 0.8 }
+            'jaktskyttebanor.geojson': { color: 'green', baseRadius: 8, fillColor: 'green', fillOpacity: 0.8 }
         }
     };
 
@@ -39,7 +39,9 @@ var Upptack_geojsonHandler = (function() {
                         pointToLayer: function(feature, latlng) {
                             var filename = getFilenameFromURL(geojsonURL);
                             var style = layerStyles[layerName][filename];
-                            return L.circleMarker(latlng, style);
+                            var zoom = map.getZoom();
+                            var radius = style.baseRadius * Math.pow(2, zoom - 5); // Anpassa denna formel efter behov
+                            return L.circleMarker(latlng, { ...style, radius: radius });
                         },
                         onEachFeature: function(feature, layer) {
                             var popupContent = '<div style="max-width: 300px; overflow-y: auto;">';
@@ -97,57 +99,55 @@ var Upptack_geojsonHandler = (function() {
 
     // Funktion för att aktivera alla lager
     function activateAllLayers() {
-        Object.keys(geojsonLayers).forEach(function(layerName) {
-            activateLayer(layerName);
+        Object.keys(layerIsActive).forEach(function(layerName) {
+            geojsonLayers[layerName].forEach(function(layer) {
+                layer.addTo(map);
+            });
+            layerIsActive[layerName] = true;
         });
     }
 
     // Funktion för att avaktivera alla lager
     function deactivateAllLayers() {
-        Object.keys(layerIsActive).forEach(function(name) {
-            if (layerIsActive[name]) {
-                geojsonLayers[name].forEach(function(layer) {
-                    map.removeLayer(layer);
-                });
-                layerIsActive[name] = false;
-            }
+        Object.keys(layerIsActive).forEach(function(layerName) {
+            geojsonLayers[layerName].forEach(function(layer) {
+                map.removeLayer(layer);
+            });
+            layerIsActive[layerName] = false;
         });
     }
 
-    function getFilenameFromURL(url) {
-        var pathArray = url.split('/');
-        var filename = pathArray[pathArray.length - 1];
-        return filename;
+    function updateMarkerRadius() {
+        var zoom = map.getZoom();
+        Object.keys(geojsonLayers).forEach(function(layerName) {
+            geojsonLayers[layerName].forEach(function(layer) {
+                layer.eachLayer(function(marker) {
+                    if (marker instanceof L.CircleMarker) {
+                        var filename = getFilenameFromURL(marker.feature.properties.geojsonURL);
+                        var style = layerStyles[layerName][filename];
+                        var radius = style.baseRadius * Math.pow(2, zoom - 5); // Anpassa denna formel efter behov
+                        marker.setRadius(radius);
+                    }
+                });
+            });
+        });
     }
 
-    // Initialisera alla lager vid start
-    fetchGeoJSONDataAndCreateLayer('Mässor', layerURLs['Mässor']);
-    fetchGeoJSONDataAndCreateLayer('Jaktkort', layerURLs['Jaktkort']);
-    fetchGeoJSONDataAndCreateLayer('Jaktskyttebanor', layerURLs['Jaktskyttebanor']);
+    map.on('zoomend', updateMarkerRadius);
 
+    function getFilenameFromURL(url) {
+        return url.substring(url.lastIndexOf('/') + 1);
+    }
+
+    // Returnerar publik API
     return {
-        toggleLayer: toggleLayer
+        toggleLayer: toggleLayer,
+        fetchGeoJSONDataAndCreateLayer: fetchGeoJSONDataAndCreateLayer
     };
 })();
 
-// Exempel på knappklick-hantering
-document.getElementById('massorButton').addEventListener('click', function() {
-    Upptack_geojsonHandler.toggleLayer('Mässor');
+// Anropa för att hämta GeoJSON-data och skapa lagren vid start
+Object.keys(layerURLs).forEach(function(layerName) {
+    Upptack_geojsonHandler.fetchGeoJSONDataAndCreateLayer(layerName, layerURLs[layerName]);
 });
 
-document.getElementById('jaktkortButton').addEventListener('click', function() {
-    Upptack_geojsonHandler.toggleLayer('Jaktkort');
-});
-
-document.getElementById('jaktskyttebanorButton').addEventListener('click', function() {
-    Upptack_geojsonHandler.toggleLayer('Jaktskyttebanor');
-});
-
-// Hantera "Visa allt" och "Rensa allt" knapparna
-document.getElementById('visaAlltButton').addEventListener('click', function() {
-    Upptack_geojsonHandler.toggleLayer('Visa_allt');
-});
-
-document.getElementById('rensaAlltButton').addEventListener('click', function() {
-    Upptack_geojsonHandler.toggleLayer('Rensa_allt');
-});
