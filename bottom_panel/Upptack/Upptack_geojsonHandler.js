3ff0692902a38e397cae61915b63409b8a39025e
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-26 20:40:25 +0200

    Update Upptack_geojsonHandler.js

--- bottom_panel/Upptack/Upptack_geojsonHandler.js
+++ bottom_panel/Upptack/Upptack_geojsonHandler.js
@@ -29,25 +29,23 @@ var Upptack_geojsonHandler = (function() {
         }
     };
 
-    // Funktion för att hämta GeoJSON-data och skapa lagret med stil
     function fetchGeoJSONDataAndCreateLayer(layerName, geojsonURLs) {
-        console.log("Fetching GeoJSON data for layer:", layerName);
         geojsonURLs.forEach(function(geojsonURL) {
-            console.log("Fetching URL:", geojsonURL);
             axios.get(geojsonURL)
                 .then(function(response) {
-                    console.log("Data fetched successfully for:", geojsonURL);
                     var geojson = response.data;
                     var layer = L.geoJSON(geojson, {
                         pointToLayer: function(feature, latlng) {
                             var filename = getFilenameFromURL(geojsonURL);
                             var style = getMarkerStyle(layerName, filename);
 
+                            if (!feature.properties.url) {
+                                feature.properties.url = geojsonURL;
+                            }
+
                             if (style.icon) {
-                                console.log("Using icon for feature at:", latlng);
                                 return L.marker(latlng, { icon: style.icon });
                             } else {
-                                console.log("Using circle marker for feature at:", latlng);
                                 return L.circleMarker(latlng, {
                                     radius: style.radius,
                                     color: style.color,
@@ -65,7 +63,6 @@ var Upptack_geojsonHandler = (function() {
                     geojsonLayers[layerName].push(layer);
 
                     if (layerIsActive[layerName]) {
-                        console.log("Adding layer to map:", layerName);
                         layer.addTo(map);
                     }
                 })
@@ -75,9 +72,7 @@ var Upptack_geojsonHandler = (function() {
         });
     }
 
-    // Funktion för att toggla lagret
     function toggleLayer(layerName) {
-        console.log("Toggling layer:", layerName);
         if (layerName === 'Visa_allt') {
             activateAllLayers();
         } else if (layerName === 'Rensa_allt') {
@@ -88,26 +83,20 @@ var Upptack_geojsonHandler = (function() {
         }
     }
 
-    // Funktion för att aktivera ett lager
     function activateLayer(layerName) {
-        console.log("Activating layer:", layerName);
         geojsonLayers[layerName].forEach(function(layer) {
             layer.addTo(map);
         });
         layerIsActive[layerName] = true;
     }
 
-    // Funktion för att aktivera alla lager
     function activateAllLayers() {
-        console.log("Activating all layers");
         Object.keys(geojsonLayers).forEach(function(layerName) {
             activateLayer(layerName);
         });
     }
 
-    // Funktion för att avaktivera alla lager
     function deactivateAllLayers() {
-        console.log("Deactivating all layers");
         Object.keys(layerIsActive).forEach(function(name) {
             if (layerIsActive[name]) {
                 geojsonLayers[name].forEach(function(layer) {
@@ -120,11 +109,9 @@ var Upptack_geojsonHandler = (function() {
 
     function getFilenameFromURL(url) {
         var pathArray = url.split('/');
-        var filename = pathArray[pathArray.length - 1];
-        return filename;
+        return pathArray[pathArray.length - 1];
     }
 
-    // Funktion för att generera popup-innehåll
     function generatePopupContent(feature) {
         var popupContent = '<div style="max-width: 300px; overflow-y: auto;">';
         var hideProperties = ['id', 'Aktualitet'];
@@ -147,23 +134,19 @@ var Upptack_geojsonHandler = (function() {
         return popupContent;
     }
 
-    // Funktion för att hämta stil baserat på zoomnivå
     function getMarkerStyle(layerName, filename) {
         var zoomLevel = map.getZoom();
-        console.log("Current zoom level:", zoomLevel);
         var style;
 
         if (zoomLevel >= 15) {
-            console.log("Using icon for zoom level >= 15");
             style = {
                 icon: L.icon({
-                    iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/ikon3.png?raw=true', // Uppdatera sökvägen här beroende på din filstruktur
-                    iconSize: [40, 40], // Justera storleken på ikonen efter behov
-                    iconAnchor: [20, 20] // Justera ikonankaret om det behövs
+                    iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/ikon3.png?raw=true',
+                    iconSize: [40, 40],
+                    iconAnchor: [20, 20]
                 })
             };
         } else {
-            console.log("Using circle marker for zoom level <", 15);
             style = {
                 radius: 5,
                 color: layerStyles[layerName][filename].color,
@@ -175,22 +158,18 @@ var Upptack_geojsonHandler = (function() {
         return style;
     }
 
-    // Funktion för att uppdatera markörer vid zoom
     function updateMarkersOnZoom() {
-        console.log("Updating markers on zoom");
         Object.keys(geojsonLayers).forEach(function(layerName) {
             geojsonLayers[layerName].forEach(function(layer) {
                 layer.eachLayer(function(marker) {
-                    if (marker.setIcon) {
-                        var zoomLevel = map.getZoom();
+                    if (marker.feature) {
                         var filename = getFilenameFromURL(marker.feature.properties.url);
                         var style = getMarkerStyle(layerName, filename);
-                        
-                        if (style.icon && zoomLevel >= 15) {
-                            console.log("Setting icon for marker at:", marker.getLatLng());
+                        var zoomLevel = map.getZoom();
+
+                        if (zoomLevel >= 15 && marker.setIcon && style.icon) {
                             marker.setIcon(style.icon);
-                        } else if (!style.icon && zoomLevel < 15) {
-                            console.log("Setting circle marker for marker at:", marker.getLatLng());
+                        } else if (zoomLevel < 15 && marker.setStyle && !style.icon) {
                             marker.setStyle({
                                 radius: style.radius,
                                 color: style.color,
@@ -204,14 +183,11 @@ var Upptack_geojsonHandler = (function() {
         });
     }
 
-    // Initialisera alla lager vid start
     fetchGeoJSONDataAndCreateLayer('Mässor', layerURLs['Mässor']);
     fetchGeoJSONDataAndCreateLayer('Jaktkort', layerURLs['Jaktkort']);
     fetchGeoJSONDataAndCreateLayer('Jaktskyttebanor', layerURLs['Jaktskyttebanor']);
 
-    // Lägg till en lyssnare för zoomhändelser
     map.on('zoomend', function() {
-        console.log("Zoom event detected");
         updateMarkersOnZoom();
     });
 
