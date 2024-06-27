2a2e6419a04c24ac4d893bbf684e2f6e874098e2
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-06-27 11:39:38 +0200

    Update Upptack_geojsonHandler.js

--- bottom_panel/Upptack/Upptack_geojsonHandler.js
+++ bottom_panel/Upptack/Upptack_geojsonHandler.js
@@ -24,15 +24,51 @@ setTimeout(function() {
         var layerStyles = {
             'Mässor': {
                 iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/upptack.png?raw=true',
-                iconSize: [40, 40]
+                iconSize: [40, 40],
+                fallbackStyle: {
+                    color: 'orange',
+                    radius: 5,
+                    fillColor: 'orange',
+                    fillOpacity: 0.8,
+                    icon: L.icon({
+                        iconUrl: 'https://static.vecteezy.com/system/resources/previews/009/887/200/non_2x/young-deer-free-png.png',
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 41],
+                        popupAnchor: [1, -34],
+                    })
+                }
             },
             'Jaktkort': {
                 iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/ikon3.png?raw=true',
-                iconSize: [40, 40]
+                iconSize: [40, 40],
+                fallbackStyle: {
+                    color: 'blue',
+                    radius: 5,
+                    fillColor: 'blue',
+                    fillOpacity: 0.8,
+                    icon: L.icon({
+                        iconUrl: 'https://e7.pngegg.com/pngimages/203/1010/png-clipart-deer-deer.png',
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 41],
+                        popupAnchor: [1, -34],
+                    })
+                }
             },
             'Jaktskyttebanor': {
-                iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/ikon3.png?raw=true',
-                iconSize: [40, 40]
+                iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/ikon_jaktskyttebanor.png?raw=true',
+                iconSize: [40, 40],
+                fallbackStyle: {
+                    color: 'green',
+                    radius: 5,
+                    fillColor: 'green',
+                    fillOpacity: 0.8,
+                    icon: L.icon({
+                        iconUrl: 'https://github.com/timothylevin/Testmiljo/blob/main/bilder/upptack_jaktskyttebanor.png?raw=true',
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 41],
+                        popupAnchor: [1, -34],
+                    })
+                }
             }
         };
 
@@ -46,6 +82,9 @@ setTimeout(function() {
                             pointToLayer: function(feature, latlng) {
                                 var style = getMarkerStyle(layerName);
                                 return L.marker(latlng, { icon: style.icon });
+                            },
+                            style: function(feature) {
+                                return getFallbackStyle(layerName);
                             },
                             onEachFeature: function(feature, layer) {
                                 var popupContent = generatePopupContent(feature);
@@ -146,18 +185,16 @@ setTimeout(function() {
                 style = {
                     // Använd en standard cirkelmarkör om ingen ikon krävs
                     // Det här kan anpassas beroende på dina behov
-                    icon: L.icon({
-                        iconUrl: 'https://static.vecteezy.com/system/resources/previews/009/887/200/non_2x/young-deer-free-png.png',
-                        iconSize: [25, 41],
-                        iconAnchor: [12, 41],
-                        popupAnchor: [1, -34],
-                    })
+                    icon: layerStyles[layerName].fallbackStyle.icon
                 };
             }
 
-            console.log("Zoom level for layer " + layerName + " is: " + zoomLevel);
-
             return style;
+        }
+
+        // Funktion för att hämta fallback-stil
+        function getFallbackStyle(layerName) {
+            return layerStyles[layerName].fallbackStyle;
         }
 
         // Initialisera alla lager vid start
@@ -184,4 +221,5 @@ setTimeout(function() {
             toggleLayer: toggleLayer
         };
     })(map); // Skicka map som parameter till självinkapslad funktion
-}, 1000); // Fördröj initialiseringen av Upptack_geojsonHandler.js med 1000 ms (1 sekund)
+}, 1000); // Fördr
+
