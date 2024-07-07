50c8b4e1a01a1bb58035ae4413f2c2eadd98efe2
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-07-07 21:29:16 +0200

    Update tabHandler.js

--- tabHandler.js
+++ tabHandler.js
@@ -1,4 +1,17 @@
 // Funktioner för att toggle väderfliken, knapparna i bottenpanelen och särskilt för kaliberkravsfliken som ger användaren två knappar för att välja vilken flik som ska visas.
+document.getElementById('tab1').addEventListener('click', function() {
+    Kartor_geojsonHandler.deactivateAllLayersKartor();
+});
+
+//Knapparna i tab1 (Upptäck) rensar geojson lager från tab2 (kartor) fliken.
+document.getElementById('tab1').addEventListener('click', function() {
+    // Kontrollera att Upptack_geojsonHandler är definierad
+    if (typeof Upptack_geojsonHandler !== 'undefined') {
+        Upptack_geojsonHandler.deactivateAllLayers();
+    } else {
+        console.error("Upptack_geojsonHandler är inte definierad.");
+    }
+});
 
 //Knapparna i tab2 (kartor) rensar geojson lager från tab1 (upptäck) fliken.
 document.getElementById('tab2').addEventListener('click', function() {
