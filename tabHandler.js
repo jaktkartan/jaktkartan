01aecfdee9ac39d1493ba5f92e452b4dea971540
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-07-16 00:21:04 +0200

    Update tabHandler.js

--- tabHandler.js
+++ tabHandler.js
@@ -8,7 +8,6 @@ document.getElementById('tab1').addEventListener('click', function() {
     openTab('tab1');
 });
 
-// Knapparna i tab2 (kartor) rensar geojson lager från tab1 (upptäck) fliken.
 document.getElementById('tab2').addEventListener('click', function() {
     if (typeof Upptack_geojsonHandler !== 'undefined') {
         Upptack_geojsonHandler.deactivateAllLayers();
@@ -122,21 +121,14 @@ function openKaliberkravTab(url) {
 // Lyssnare för klick utanför flikar och panelknappar
 document.addEventListener('click', function(event) {
     var tabContent = document.getElementById('tab-content');
-    var panelButtons = document.querySelectorAll('.panel-button');
 
-    // Kontrollera om klicket är utanför tab-content och panel-buttons
+    // Kontrollera om klicket är utanför tab-content
     if (!tabContent.contains(event.target) && !event.target.closest('.panel-button')) {
         resetTabs();
     }
 });
 
-// Funktion för att stänga flikinnehåll
-function closeTabContent() {
-    var tabContent = document.getElementById('tab-content');
-    tabContent.style.display = 'none';
-}
-
-// Lyssnare för när sidan laddas
-document.addEventListener('DOMContentLoaded', function() {
-    // Lägg till annan kod för att hantera DOMContentLoaded om nödvändigt
+// Lyssnare för att förhindra att klick inom tab-content stänger flikarna
+document.getElementById('tab-content').addEventListener('click', function(event) {
+    event.stopPropagation();
 });
