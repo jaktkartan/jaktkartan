7de4e4c6546e6a4293992e5c9cbce9bfcbddef7b
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-07-16 01:26:04 +0200

    Update kartor_flikbeteende.js

--- bottom_panel/Kartor/kartor_flikbeteende.js
+++ bottom_panel/Kartor/kartor_flikbeteende.js
@@ -1,5 +1,9 @@
 // Funktion för att skapa kartor-fliken
 function openKartor() {
+    // Hitta och rensa tab2 innehåll
+    const tabPane = document.getElementById('tab1');
+    tabPane.innerHTML = '';
+
     // Skapa en container div för att centrera innehållet
     const container = document.createElement('div');
     container.style.display = 'flex';
@@ -8,18 +12,14 @@ function openKartor() {
     container.style.height = '100vh';
     container.style.overflow = 'hidden'; // Förhindra scrollning
 
-    // Skapa tab-pane
-    const tabPane = document.createElement('div');
-    tabPane.className = 'tab-pane'; // Se till att detta klassnamn är definierat i din CSS
-
     // Skapa knapp-container
     const buttonContainer = document.createElement('div');
-    buttonContainer.className = 'button-container'; // Se till att detta klassnamn är definierat i din CSS
+    buttonContainer.className = 'button-container';
 
     // Definiera knapparna med deras respektive egenskaper
     const buttons = [
         {
-            className: 'styled-button', // Se till att denna klass är definierad i din CSS
+            className: 'styled-button',
             onclick: "Kartor_geojsonHandler.toggleLayer('Allmän jakt: Däggdjur', ['https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_daggdjur/geojsonfiler/Rvjaktilvdalenskommun_1.geojson', 'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_daggdjur/geojsonfiler/Allman_jakt_daggdjur_2.geojson'])",
             imgSrc: 'bottom_panel/Kartor/bilder/daggdjurikon.png',
             imgAlt: 'Allmän jakt: Däggdjur'
