7b0bc56f0840be7d1ce321f222e9b345d918b0e6
Maps.jaktkartan <167991082+timothylevin@users.noreply.github.com>
2024-07-17 00:02:31 +0200

    Update kartor_flikbeteende.js

--- bottom_panel/Kartor/kartor_flikbeteende.js
+++ bottom_panel/Kartor/kartor_flikbeteende.js
@@ -1,124 +1,198 @@
-function openKartor() {
-    const tabPane = document.getElementById('tab2');
-    if (!tabPane) {
-        console.error('Tab pane for kartor not found.');
-        return;
-    }
+var Kartor_geojsonHandler = (function() {
+    // Status för vilka lager som är aktiva
+    var layerIsActive = {
+        'Allmän jakt: Däggdjur': false,
+        'Allmän jakt: Fågel': false,
+        'Älgjaktskartan': false,
+        'Älgjaktsområden': false  // Lägg till WMS-lagret
+    };
 
-    tabPane.innerHTML = '';
+    // Objekt som lagrar GeoJSON-lager för varje lager
+    var geojsonLayers = {
+        'Allmän jakt: Däggdjur': [],
+        'Allmän jakt: Fågel': [],
+        'Älgjaktskartan': [],
+        'Älgjaktsområden': []  // Lägg till WMS-lagret
+    };
 
-    const container = document.createElement('div');
-    container.style.display = 'flex';
-    container.style.flexDirection = 'column';
-    container.style.justifyContent = 'center';
-    container.style.alignItems = 'center';
-    container.style.height = '100vh';
-    container.style.overflow = 'hidden';
-
-    const buttonContainer = document.createElement('div');
-    buttonContainer.className = 'button-container';
-    buttonContainer.style.display = 'flex';
-
-    const buttons = [
-        {
-            className: 'styled-button',
-            onclick: "Kartor_geojsonHandler.toggleLayer('Allmän jakt: Däggdjur', ['https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_daggdjur/geojsonfiler/Rvjaktilvdalenskommun_1.geojson', 'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_daggdjur/geojsonfiler/Allman_jakt_daggdjur_2.geojson'])",
-            imgSrc: 'bottom_panel/Kartor/bilder/daggdjurikon.png',
-            imgAlt: 'Allmän jakt: Däggdjur'
+    // Stilar för olika lager och GeoJSON-filer
+    var layerStyles = {
+        'Allmän jakt: Däggdjur': {
+            'Rvjaktilvdalenskommun_1.geojson': { fillColor: 'orange', color: 'rgb(50, 94, 88)', weight: 2, dashArray: '5, 10', fillOpacity: 0.001 },
+            'Allman_jakt_daggdjur_2.geojson': { fillColor: 'blue', color: 'rgb(50, 94, 88)', weight: 2, fillOpacity: 0.001 }
         },
-        {
-            className: 'styled-button',
-            onclick: "Kartor_geojsonHandler.toggleLayer('Allmän jakt: Fågel', ['https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_Fagel/geojsonfiler/Lnsindelning_1.geojson', 'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_Fagel/geojsonfiler/Grnsfrripjaktilvdalenskommun_2.geojson', 'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_Fagel/geojsonfiler/GrnslvsomrdetillFinland_5.geojson', 'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_Fagel/geojsonfiler/NedanfrLappmarksgrnsen_3.geojson', 'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Allman_jakt_Fagel/geojsonfiler/OvanfrLapplandsgrnsen_4.geojson'])",
-            imgSrc: 'bottom_panel/Kartor/bilder/fagelikon.png',
-            imgAlt: 'Allmän jakt: Fågel'
+        'Allmän jakt: Fågel': {
+            'Lnsindelning_1.geojson': { fillColor: 'yellow', color: 'rgb(50, 94, 88)', weight: 2, fillOpacity: 0.001 },
+            'Grnsfrripjaktilvdalenskommun_2.geojson': { fillColor: 'rgb(50, 94, 88)', color: 'rgb(50, 94, 88)', weight: 2, dashArray: '5, 10', fillOpacity: 0.001 },
+            'GrnslvsomrdetillFinland_5.geojson': { fillColor: 'blue', color: 'blue', weight: 8, fillOpacity: 0.5, dashArray: '5, 10' },
+            'NedanfrLappmarksgrnsen_3.geojson': { fillColor: '#fdae61', color: '#edf8e9', weight: 2, fillOpacity: 0.5, dashArray: '5, 10' },
+            'OvanfrLapplandsgrnsen_4.geojson': { fillColor: '#a6d96a', color: '#edf8e9', weight: 2, fillOpacity: 0.5 }
         },
-        {
-            className: 'styled-button',
-            id: 'elgjaktskartan-button',
-            imgSrc: 'bottom_panel/Kartor/bilder/algikon.png',
-            imgAlt: 'Älgjaktskartan'
+        'Älgjaktskartan': {
+            'lgjaktJakttider_1.geojson': {
+                style: (function() {
+                    // Färgschema för jakttider
+                    var colorScale = [
+                        '#ffd54f', '#72d572', '#ff7043', '#1ba01b', '#20beea',
+                        '#81d4fa', '#ab47bc', '#e9a6f4', '#78909c', '#9c8019', '#b5f2b5'
+                    ];
+                    var jakttidToColor = {};
+                    var currentIndex = 0;
+                    
+                    // Returnerar en funktion som tilldelar färg baserat på jakttid
+                    return function(feature) {
+                        var jakttid = feature.properties['jakttid'];
+                        if (!jakttidToColor[jakttid]) {
+                            jakttidToColor[jakttid] = colorScale[currentIndex];
+                            currentIndex = (currentIndex + 1) % colorScale.length;
+                        }
+                        return { fillColor: jakttidToColor[jakttid], color: 'rgb(50, 94, 88)', weight: 2, fillOpacity: 0.5 };
+                    };
+                })()
+            },
+            'Srskiltjakttidsfnster_3.geojson': { fillColor: 'purple', color: 'purple', weight: 2 },
+            'Omrdemedbrunstuppehll_2.geojson': { fill: false, color: 'black', weight: 7, dashArray: '5, 10' },
+            'Kirunakommunnedanodlingsgrns_4.geojson': { fillColor: 'pink', color: 'pink', weight: 2 }
         }
-    ];
+    };
 
-    buttons.forEach(button => {
-        const btn = document.createElement('button');
-        btn.className = button.className;
-        btn.setAttribute('onclick', button.onclick || '');
-        btn.id = button.id || '';
-
-        const img = document.createElement('img');
-        img.src = button.imgSrc;
-        img.alt = button.imgAlt;
-
-        btn.appendChild(img);
-        buttonContainer.appendChild(btn);
-    });
-
-    tabPane.appendChild(buttonContainer);
-
-    container.appendChild(tabPane);
-    document.body.appendChild(container);
-
-    const elkMapButton = document.getElementById('elgjaktskartan-button');
-
-    elkMapButton.addEventListener('click', function(event) {
-        event.stopPropagation();
-        showElkMapOptions();
-    });
-
-    function showElkMapOptions() {
-        buttonContainer.innerHTML = '';
-
-        const elkJaktsomradenButton = document.createElement('button');
-        elkJaktsomradenButton.className = 'styled-button';
-        elkJaktsomradenButton.onclick = function() {
-            Kartor_geojsonHandler.toggleLayer('Älgjaktsområden', []);
-        };
-
-        const elkAlternativButton = document.createElement('button');
-        elkAlternativButton.className = 'styled-button';
-        elkAlternativButton.onclick = function() {
-            Kartor_geojsonHandler.toggleLayer('Älgjaktskartan', [
-                'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Algjaktskartan/geojsonfiler/lgjaktJakttider_1.geojson',
-                'https://raw.githubusercontent.com/timothylevin/Testmiljo/main/bottom_panel/Kartor/Algjaktskartan/geojsonfiler/Omrdemedbrunstuppehll_2.geojson'
-            ]);
-            restoreOriginalButtons();
-        };
-
-        const jaktsomradenImg = document.createElement('img');
-        jaktsomradenImg.src = 'bottom_panel/Kartor/bilder/Algjaktsomraden_ikon.png';
-        jaktsomradenImg.alt = 'Älgjaktsområden';
-        elkJaktsomradenButton.appendChild(jaktsomradenImg);
-
-        const alternativImg = document.createElement('img');
-        alternativImg.src = 'bottom_panel/Kartor/bilder/Algjaktskartan_ikon.png';
-        alternativImg.alt = 'Älgjaktsalternativ';
-        elkAlternativButton.appendChild(alternativImg);
-
-        buttonContainer.appendChild(elkJaktsomradenButton);
-        buttonContainer.appendChild(elkAlternativButton);
-    }
-
-    function restoreOriginalButtons() {
-        buttonContainer.innerHTML = '';
-
-        buttons.forEach(button => {
-            if (button.id !== 'elgjaktskartan-button') {
-                const btn = document.createElement('button');
-                btn.className = button.className;
-                btn.setAttribute('onclick', button.onclick || '');
-
-                const img = document.createElement('img');
-                img.src = button.imgSrc;
-                img.alt = button.imgAlt;
-
-                btn.appendChild(img);
-                buttonContainer.appendChild(btn);
+    // Funktion för att hämta GeoJSON-data och skapa ett lager
+    function fetchGeoJSONDataAndCreateLayer(layerName, geojsonURLs) {
+        // Inaktivera andra lager om de är aktiva
+        Object.keys(layerIsActive).forEach(function(name) {
+            if (name !== layerName && layerIsActive[name]) {
+                toggleLayer(name, geojsonLayers[name].map(function(layer) {
+                    return layer.options.url;
+                }));
             }
         });
 
-        buttonContainer.appendChild(elkMapButton);
+        // Hämta GeoJSON-data från URL och skapa lager
+        geojsonURLs.forEach(function(geojsonURL) {
+            axios.get(geojsonURL)
+                .then(function(response) {
+                    var geojson = response.data;
+                    
+                    // Skapa GeoJSON-lager med stil och klickhändelse
+                    var layer = L.geoJSON(geojson, {
+                        style: function(feature) {
+                            var filename = getFilenameFromURL(geojsonURL);
+                            return layerStyles[layerName][filename].style ? layerStyles[layerName][filename].style(feature) : layerStyles[layerName][filename];
+                        },
+                        onEachFeature: function(feature, layer) {
+                            addClickHandlerToLayer(layer);
+                        }
+                    });
+
+                    geojsonLayers[layerName].push(layer);
+
+                    // Lägg till lagret på kartan om det är aktivt
+                    if (layerIsActive[layerName]) {
+                        layer.addTo(map);
+                    }
+                })
+                .catch(function() {
+                    console.error("Error fetching GeoJSON data.");
+                });
+        });
+
+        // Markera lagret som aktivt
+        layerIsActive[layerName] = true;
+        updateFAB(layerName, true);
     }
 
-    console.log('Kartor tab created and added to body');
-}
+    // Funktion för att ladda och hantera WMS-lager
+    function loadWMSLayer(layerName) {
+        var wmsUrl = 'https://geodata.nationell.geodataportalen.se/land/ows?service=WMS&request=GetMap&layers=algar:algjaktsomraden&styles=&format=image/png&transparent=true&version=1.3.0';
+        var wmsLayer = L.tileLayer.wms(wmsUrl, {
+            layers: 'algar:algjaktsomraden',
+            format: 'image/png',
+            transparent: true
+        });
+
+        // Lägg till lagret om det inte redan är aktivt
+        if (!layerIsActive[layerName]) {
+            map.addLayer(wmsLayer);
+            geojsonLayers[layerName].push(wmsLayer);
+            layerIsActive[layerName] = true;
+            updateFAB(layerName, true);
+        } else {
+            map.removeLayer(wmsLayer);
+            geojsonLayers[layerName] = [];
+            layerIsActive[layerName] = false;
+            updateFAB(layerName, false);
+        }
+    }
+
+    // Funktion för att växla (aktivera/inaktivera) lager
+    function toggleLayer(layerName, geojsonURLs) {
+        if (layerName === 'Älgjaktsområden') {
+            loadWMSLayer(layerName);
+        } else {
+            if (!layerIsActive[layerName]) {
+                fetchGeoJSONDataAndCreateLayer(layerName, geojsonURLs);
+            } else {
+                geojsonLayers[layerName].forEach(function(layer) {
+                    map.removeLayer(layer);  // Ta bort lager från kartan
+                });
+
+                geojsonLayers[layerName] = [];
+                layerIsActive[layerName] = false;
+                updateFAB(layerName, false);
+            }
+        }
+    }
+
+    // Ny funktion för att inaktivera alla lager
+    function deactivateAllLayersKartor() {
+        console.log("Deactivating all layers.");
+        Object.keys(layerIsActive).forEach(function(layerName) {
+            if (layerIsActive[layerName]) {
+                console.log("Deactivating layer:", layerName);
+                geojsonLayers[layerName].forEach(function(layer) {
+                    map.removeLayer(layer); // Ta bort lager från kartan
+                });
+                geojsonLayers[layerName] = []; // Rensa listan med lager
+                layerIsActive[layerName] = false; // Markera som inaktiv
+                updateFAB(layerName, false);
+            }
+        });
+    }
+
+    // Funktion för att få filnamnet från en URL
+    function getFilenameFromURL(url) {
+        return url.split('/').pop();
+    }
+
+    // Funktion för att uppdatera FAB-knappen baserat på lagrets tillstånd
+    function updateFAB(layerName, show) {
+        var fabId = getFABId(layerName);
+        var fabButton = document.getElementById(fabId);
+        if (fabButton) {
+            fabButton.style.display = show ? 'block' : 'none';
+        }
+    }
+
+    // Hjälpfunktion för att få FAB-knappens ID baserat på lagrets namn
+    function getFABId(layerName) {
+        switch(layerName) {
+            case 'Allmän jakt: Däggdjur':
+                return 'fab-daggdjur';
+            case 'Allmän jakt: Fågel':
+                return 'fab-fagel';
+            case 'Älgjaktskartan':
+                return 'fab-alg';
+            case 'Älgjaktsområden':  // Lägg till WMS-lagret
+                return 'fab-algjaktsomraden';
+            default:
+                return '';
+        }
+    }
+
+    // Exponerar funktionerna för att växla lager och hämta GeoJSON-data
+    return {
+        toggleLayer: toggleLayer,
+        fetchGeoJSONDataAndCreateLayer: fetchGeoJSONDataAndCreateLayer,
+        deactivateAllLayersKartor: deactivateAllLayersKartor  // Exponerar den nya funktionen
+    };
+})();
