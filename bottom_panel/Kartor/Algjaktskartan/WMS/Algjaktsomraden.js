3f3e0f4835f469fc0a4ca16cb05eb3cd03a3ead0
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-07-21 09:02:53 +0200

    Update Algjaktsomraden.js

--- bottom_panel/Kartor/Algjaktskartan/WMS/Algjaktsomraden.js
+++ bottom_panel/Kartor/Algjaktskartan/WMS/Algjaktsomraden.js
@@ -1,4 +1,3 @@
-// funktionen triggas från bottom_panel/Kartor/kartor_flikbeteende.js
 function loadElgjaktsomradenWMS() {
     // Funktion för att generera en slumpmässig färg i en naturlig nyans
     function getRandomColor() {
@@ -10,60 +9,49 @@ function loadElgjaktsomradenWMS() {
     // Färgcache för att bevara färger för varje feature
     var colorCache = {};
 
-    // Funktion för att lägga till feature layer
-    function addFeatureLayer() {
-        var featureLayer = L.esri.featureLayer({
-            url: 'https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/rest/services/Jaktadm/lst_jaktadm_visning/MapServer/0',
-            style: function (feature) {
-                // Om feature inte redan har en färg i cache, generera och spara i cache
-                if (!colorCache[feature.id]) {
-                    colorCache[feature.id] = getRandomColor();
-                }
-                return {
-                    color: colorCache[feature.id], // Kantfärg
-                    weight: 2, // Kantens tjocklek
-                    opacity: 1, // Kantens opacitet
-                    fillColor: colorCache[feature.id], // Fyllningsfärg
-                    fillOpacity: 0.5 // Konstant transparens på fyllningen
-                };
-            },
-            onEachFeature: function (feature, layer) {
-                // Bygg en HTML-tabell med attributdata
-                var popupContent = '<table class="popup-table">';
-                if (feature.properties) {
-                    for (var key in feature.properties) {
-                        if (feature.properties.hasOwnProperty(key)) {
-                            popupContent += '<tr><th>' + key + '</th><td>' + feature.properties[key] + '</td></tr>';
-                        }
-                    }
-                }
-                popupContent += '</table>';
-                
-                // Bind popup med HTML-tabellen
-                layer.bindPopup(popupContent);
-            }
-        }).addTo(window.map);
+    // Caching inställningar
+    var cache = {};
 
-        // Funktion som döljer eller visar lagret baserat på zoomnivå
-        function updateLayerVisibility() {
-            var zoomLevel = window.map.getZoom();
-            if (zoomLevel >= 13) { // Visa lagret vid zoomnivå 13 eller högre
-                if (!window.map.hasLayer(featureLayer)) {
-                    featureLayer.addTo(window.map);
-                }
-            } else {
-                if (window.map.hasLayer(featureLayer)) {
-                    window.map.removeLayer(featureLayer);
-                }
-            }
+    function getTileUrl(tilePoint) {
+        var key = tilePoint.z + '/' + tilePoint.x + '/' + tilePoint.y;
+        if (cache[key]) {
+            return cache[key];
         }
-
-        // Lägg till en "zoomend" lyssnare för att uppdatera lagrets synlighet när användaren zoomar
-        window.map.on('zoomend', updateLayerVisibility);
-
-        // Initial kontroll för att ställa in lagrets synlighet när kartan först laddas
-        updateLayerVisibility();
+        var url = L.Util.template('https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/services/Jaktadm/lst_jaktadm_visning/MapServer/WmsServer', tilePoint);
+        cache[key] = url;
+        return url;
     }
 
-    addFeatureLayer();
+    // Skapa WMS-lagret
+    var wmsLayer = L.tileLayer.wms('https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/services/Jaktadm/lst_jaktadm_visning/MapServer/WmsServer', {
+        layers: '0',
+        format: 'image/png',
+        transparent: true,
+        attribution: 'Lantmäteriet',
+        tileSize: 512,
+        detectRetina: true
+    });
+
+    // Lägg till WMS-lagret till kartan
+    window.map.addLayer(wmsLayer);
+
+    // Funktion som döljer eller visar lagret baserat på zoomnivå
+    function updateLayerVisibility() {
+        var zoomLevel = window.map.getZoom();
+        if (zoomLevel >= 13) { // Visa lagret vid zoomnivå 13 eller högre
+            if (!window.map.hasLayer(wmsLayer)) {
+                window.map.addLayer(wmsLayer);
+            }
+        } else {
+            if (window.map.hasLayer(wmsLayer)) {
+                window.map.removeLayer(wmsLayer);
+            }
+        }
+    }
+
+    // Lägg till en "zoomend" lyssnare för att uppdatera lagrets synlighet när användaren zoomar
+    window.map.on('zoomend', updateLayerVisibility);
+
+    // Initial kontroll för att ställa in lagrets synlighet när kartan först laddas
+    updateLayerVisibility();
 }
