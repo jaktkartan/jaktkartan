256b6f068461d0e3f4f8c5523f61622b0195c1af
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-07-22 11:15:21 +0200

    Update Kartor_geojsonHandler.js

--- bottom_panel/Kartor/Kartor_geojsonHandler.js
+++ bottom_panel/Kartor/Kartor_geojsonHandler.js
@@ -13,12 +13,41 @@ var Kartor_geojsonHandler = (function() {
         'Älgjaktsområden': null
     };
 
-    var layerStyles = {
-        'Allmän jakt: Däggdjur': { /* Ditt lager stil */ },
-        'Allmän jakt: Fågel': { /* Ditt lager stil */ },
-        'Älgjaktskartan': { /* Ditt lager stil */ }
-    };
+     var layerStyles = {
+        'Allmän jakt: Däggdjur': {
+            'Rvjaktilvdalenskommun_1.geojson': { fillColor: 'orange', color: 'rgb(50, 94, 88)', weight: 2, dashArray: '5, 10', fillOpacity: 0.001 },
+            'Allman_jakt_daggdjur_2.geojson': { fillColor: 'blue', color: 'rgb(50, 94, 88)', weight: 2, fillOpacity: 0.001 }
+        },
+        'Allmän jakt: Fågel': {
+            'Lnsindelning_1.geojson': { fillColor: 'yellow', color: 'rgb(50, 94, 88)', weight: 2, fillOpacity: 0.001 },
+            'Grnsfrripjaktilvdalenskommun_2.geojson': { fillColor: 'rgb(50, 94, 88)', color: 'rgb(50, 94, 88)', weight: 2, dashArray: '5, 10', fillOpacity: 0.001 },
+            'GrnslvsomrdetillFinland_5.geojson': { fillColor: 'blue', color: 'blue', weight: 8, fillOpacity: 0.5, dashArray: '5, 10' },
+            'NedanfrLappmarksgrnsen_3.geojson': { fillColor: '#fdae61', color: '#edf8e9', weight: 2, fillOpacity: 0.5, dashArray: '5, 10' },
+            'OvanfrLapplandsgrnsen_4.geojson': { fillColor: '#a6d96a', color: '#edf8e9', weight: 2, fillOpacity: 0.5 }
+        },
+        'Älgjaktskartan': {
+            'lgjaktJakttider_1.geojson': {
+                style: (function() {
+                    var colorScale = [
+                        '#ffd54f', '#72d572', '#ff7043', '#1ba01b', '#20beea',
+                        '#81d4fa', '#ab47bc', '#e9a6f4', '#78909c', '#9c8019', '#b5f2b5'
+                    ];
+                    var jakttidToColor = {};
+                    var currentIndex = 0;
 
+                    return function(feature) {
+                        var jakttid = feature.properties['jakttid'];
+                        if (!jakttidToColor[jakttid]) {
+                            jakttidToColor[jakttid] = colorScale[currentIndex];
+                            currentIndex = (currentIndex + 1) % colorScale.length;
+                        }
+                        return { fillColor: jakttidToColor[jakttid], color: 'rgb(50, 94, 88)', weight: 2, fillOpacity: 0.5 };
+                    };
+                })()
+            },
+            'Omrdemedbrunstuppehll_2.geojson': { fill: false, color: 'black', weight: 7, dashArray: '5, 10' }
+        }
+    };
     async function fetchGeoJSONDataAndCreateLayer(layerName, geojsonURLs) {
         for (const geojsonURL of geojsonURLs) {
             try {
