d08ab184a97aeeb73b3e9d79eeef132dd12eefb0
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-07-23 10:16:43 +0200

    Update Upptack_geojsonHandler.js

--- bottom_panel/Upptack/Upptack_geojsonHandler.js
+++ bottom_panel/Upptack/Upptack_geojsonHandler.js
@@ -241,8 +241,8 @@ setTimeout(function() {
         return {
             toggleLayer: toggleLayer,
             deactivateAllLayers: deactivateAllLayers,
-            activateAllLayers: activateAllLayers, // Lägg till denna rad för att exportera funktionen
-            activateLayer: activateLayer // Lägg till denna rad för att exportera funktionen
+            activateAllLayers: activateAllLayers,
+            activateLayer: activateLayer
         };
     })(map);
 }, 1000);
