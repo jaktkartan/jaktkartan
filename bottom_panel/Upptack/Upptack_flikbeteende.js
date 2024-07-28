9f87f59e7de5c37ebed11d98e0fcced3cbdebc41
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-07-29 00:16:11 +0200

    Update Upptack_flikbeteende.js

--- bottom_panel/Upptack/Upptack_flikbeteende.js
+++ bottom_panel/Upptack/Upptack_flikbeteende.js
@@ -18,7 +18,7 @@ function openUpptack() {
     // Skapa och lägg till en rubrik högst upp
     const header = document.createElement('h1');
     header.textContent = 'Upptäck!';
-    header.className = 'tab1-3-header-title';
+    header.className = 'tab1-2-header-title';
     tabPane.appendChild(header);
 
     // Skapa en container div för att centrera innehållet
