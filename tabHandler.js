62cf96147bf6af6ef3c23161207d8ed31d09afbc
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-07-30 00:34:25 +0200

    Update tabHandler.js

--- tabHandler.js
+++ tabHandler.js
@@ -1,104 +1,3 @@
-// Lägg till stilar dynamiskt
-const style = document.createElement('style');
-style.textContent = `
-@keyframes slideIn {
-    from {
-        transform: translateY(100%);
-    }
-    to {
-        transform: translateY(0);
-    }
-}
-
-@keyframes slideOut {
-    from {
-        transform: translateY(0);
-    }
-    to {
-        transform: translateY(100%);
-    }
-}
-
-.tab-pane {
-    position: fixed;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 95%;
-    max-height: 40%;
-    background-color: #fff;
-    border-top: 5px solid #fff;
-    border-left: 5px solid #fff;
-    border-right: 5px solid #fff;
-    padding: 10px;
-    box-shadow: 0 0 10px rgba(0,0,0,0.1);
-    z-index: 1000;
-    overflow-y: auto;
-    word-wrap: break-word;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    font-family: 'Roboto', sans-serif;
-    color: rgb(50, 94, 88);
-    transform: translateY(100%);
-    transition: transform 0.5s ease-in-out;
-}
-
-.tab-pane.show {
-    animation: slideIn 0.5s forwards;
-}
-
-.tab-pane.hide {
-    animation: slideOut 0.5s forwards;
-}
-
-/* Media query för att göra tabbarna smalare på större skärmar */
-@media (min-width: 768px) {
-    .tab-pane {
-        width: 70%;
-    }
-}
-
-@media (min-width: 1200px) {
-    .tab-pane {
-        width: 40%;
-    }
-}
-
-/* Specifika stilar för #tab1 och #tab2 */
-#tab1.tab-pane, #tab2.tab-pane {
-    height: 15vh; /* Specifik höjd för tab1 och tab2 */
-    display: flex; /* Lägg till flexbox layout här också */
-    flex-direction: column; /* Säkerställ att barnen ordnas i kolumner */
-    padding: 0px;
-    justify-content: flex-end; /* Placera innehållet längst ner */
-}
-
-.tab1-2-header-title {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    margin: 0;
-    padding: 0;
-    font-size: 24px;
-    color: rgb(50, 94, 88);
-}
-
-#tab1 .button-container, #tab2 .button-container {
-    margin-top: 5vh; /* Glipa mellan knapparnas överkant och flikens överkant */
-}
-
-/* Skrollbara flikar */
-#tab3.tab-pane, #tab5.tab-pane {
-    height: 50vh; /* Specifik höjd för tab3 och tab5 */
-    display: flex; /* Flexbox layout */
-    flex-direction: column; /* Kolumnlayout */
-    padding: 0px;
-    justify-content: flex-start; /* Placera innehållet längst upp */
-    overflow-y: auto; /* Gör fliken skrollbar */
-}
-`;
-document.head.appendChild(style);
-
 // Knappen tab1 (upptäck) rensar geojson-lager och WMS från tab2 (kartor) fliken.
 document.getElementById('tab1').addEventListener('click', function() {
     if (typeof Kartor_geojsonHandler !== 'undefined') {
@@ -121,29 +20,20 @@ document.getElementById('tab2').addEventListener('click', function() {
 function resetTabs() {
     var tabs = document.getElementsByClassName('tab-pane');
     for (var i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove('show');
-        tabs[i].classList.add('hide');
+        clearTabPaneContent(tabs[i]);
+        tabs[i].style.display = 'none';
     }
-    setTimeout(function() {
-        var tabs = document.getElementsByClassName('tab-pane');
-        for (var i = 0; i < tabs.length; i++) {
-            clearTabPaneContent(tabs[i]);
-            tabs[i].style.display = 'none';
-        }
-        var tabContent = document.getElementById('tab-content');
-        tabContent.style.display = 'none';
-    }, 500); // Vänta tills animationen är klar
+    var tabContent = document.getElementById('tab-content');
+    tabContent.style.display = 'none';
 }
 
 // Funktion för att öppna en flik
 function openTab(tabId, url) {
     resetTabs();
     var tab = document.getElementById(tabId);
+    tab.style.display = 'block';
     var tabContent = document.getElementById('tab-content');
     tabContent.style.display = 'block';
-    tab.style.display = 'block';
-    tab.classList.remove('hide');
-    tab.classList.add('show');
 
     if (tabId === 'tab1') {
         openUpptack();
@@ -158,14 +48,12 @@ function openTab(tabId, url) {
     }
 }
 
-
 function clearTabPaneContent(tabPane) {
     while (tabPane.firstChild) {
         tabPane.removeChild(tabPane.firstChild);
     }
 }
 
-// Funktion för att öppna "Kaliberkrav" fliken
 function openKaliberkravTab(url) {
     var tabContent = document.getElementById('tab-content');
     var tab = document.createElement('div');
@@ -177,24 +65,10 @@ function openKaliberkravTab(url) {
         .then(html => {
             tab.innerHTML += html;
             tab.style.display = 'block';
-            tab.classList.add('show');
         })
         .catch(error => {
             console.error('Error fetching Kaliberkrav content:', error);
         });
-}
-
-// Andra flikfunktioner
-function openUpptack() {
-    // Innehåll för fliken "Upptäck"
-}
-
-function openKartor() {
-    // Innehåll för fliken "Kartor"
-}
-
-function openJaktbartIdag() {
-    // Innehåll för fliken "Kartor"
 }
 
 
