7893912e244b48ab3619c2a67d03ecf6857608b5
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-08-04 21:20:47 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -214,7 +214,7 @@ function generatePopupContent(properties) {
         content += `
             <p>
                 <button class="link-button" onclick="window.open('${länURLTabell[förvaltandelän]}', '_blank')">
-                    Besök ${förvaltandelän}
+                    Jakttid: ${förvaltandelän}
                     <img src="bilder/extern_link.png" alt="Extern länk" class="custom-image">
                 </button>
             </p>`;
