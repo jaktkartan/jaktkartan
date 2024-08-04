701840ea09c5bd4d6ca6c9742059f86f151b6f01
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-08-04 16:24:21 +0200

    Update Algjaktsomraden.js

--- bottom_panel/Kartor/Algjaktskartan/WMS/Algjaktsomraden.js
+++ bottom_panel/Kartor/Algjaktskartan/WMS/Algjaktsomraden.js
@@ -1,71 +1,149 @@
-function loadElgjaktsomradenWMS() {
-    // Funktion för att generera en slumpmässig färg i en naturlig nyans
-    function getRandomColor() {
-        var hue = Math.floor(Math.random() * 360); // Färgton
-        var lightness = Math.floor(Math.random() * 40) + 40; // Ljushet från 40 till 80
-        return `hsl(${hue}, 70%, ${lightness}%)`;
+function loadElgjaktWMS(add) {
+    if (add) {
+        if (currentWMSLayer) {
+            console.log('Layer is already added. No action taken.');
+            return;
+        }
+        checkZoomLevel();
+        console.log('Adding Älgjaktsområden layer.');
+        currentWMSLayer = L.tileLayer.wms('https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/services/Jaktadm/lst_jaktadm_visning/MapServer/WMSServer', {
+            layers: '2',
+            format: 'image/png',
+            transparent: true,
+            attribution: 'Länsstyrelsen',
+            opacity: 0.5
+        }).addTo(map);
+
+        wmsClickHandler = function(e) {
+            handleWMSClick(e);
+        };
+        map.on('click', wmsClickHandler);
+
+        // Lägg till händelse för att övervaka zoomnivån
+        map.on('zoomend', checkZoomLevel);
+
+        console.log("WMS layer added to map:", currentWMSLayer);
+        updateFAB('Älgjaktsområden', true); // Säkerställ att FAB-knappen visas
+    } else {
+        if (currentWMSLayer) {
+            console.log('Removing Älgjaktsområden layer.');
+            map.off('click', wmsClickHandler);
+            map.removeLayer(currentWMSLayer);
+            currentWMSLayer = null;
+            wmsClickHandler = null;
+            hideZoomMessage(); // Dölj meddelandet när lagret tas bort
+            map.off('zoomend', checkZoomLevel); // Ta bort händelsen för zoomnivån
+            updateFAB('Älgjaktsområden', false); // Säkerställ att FAB-knappen döljs
+        }
     }
+}
 
-    // Färgcache för att bevara färger för varje feature
-    var colorCache = {};
+function loadAlgforvaltningsomradeWMS(add) {
+    if (add) {
+        if (currentWMSLayer) {
+            console.log('Layer is already added. No action taken.');
+            return;
+        }
+        checkZoomLevel();
+        console.log('Adding Algforvaltningsomrade layer.');
+        currentWMSLayer = L.tileLayer.wms('https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/services/Jaktadm/lst_jaktadm_visning/MapServer/WMSServer', {
+            layers: '1',
+            format: 'image/png',
+            transparent: true,
+            attribution: 'Länsstyrelsen',
+            opacity: 0.5
+        }).addTo(map);
 
-    // Skapa FeatureLayer
-    var featureLayer = L.esri.featureLayer({
-        url: 'https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/rest/services/Jaktadm/lst_jaktadm_visning/MapServer/0',
-        style: function (feature) {
-            // Om feature inte redan har en färg i cache, generera och spara i cache
-            if (!colorCache[feature.id]) {
-                colorCache[feature.id] = getRandomColor();
-            }
-            return {
-                color: colorCache[feature.id], // Kantfärg
-                weight: 2, // Kantens tjocklek
-                opacity: 1, // Kantens opacitet
-                fillColor: colorCache[feature.id], // Fyllningsfärg
-                fillOpacity: 0.5 // Konstant transparens på fyllningen
-            };
-        },
-        onEachFeature: function (feature, layer) {
-            // Bygg en HTML-tabell med attributdata
-            var popupContent = '<table class="popup-table">';
-            if (feature.properties) {
-                for (var key in feature.properties) {
-                    if (feature.properties.hasOwnProperty(key)) {
-                        popupContent += '<tr><th>' + key + '</th><td>' + feature.properties[key] + '</td></tr>';
-                    }
-                }
-            }
-            popupContent += '</table>';
-            
-            // Bind popup med HTML-tabellen
-            layer.bindPopup(popupContent);
-        },
-        // Hämta endast de polygoner som finns inom det aktuella kartutsnittet
-        where: "1=1",
-        useCors: false
-    });
+        wmsClickHandler = function(e) {
+            handleWMSClick(e);
+        };
+        map.on('click', wmsClickHandler);
 
-    // Lägg till FeatureLayer till kartan
-    window.map.addLayer(featureLayer);
+        // Lägg till händelse för att övervaka zoomnivån
+        map.on('zoomend', checkZoomLevel);
 
-    // Funktion som uppdaterar datalagret baserat på kartans bounding box
-    function updateFeatureLayer() {
-        var bounds = window.map.getBounds();
-        var query = featureLayer.createQuery();
-        query.intersects(bounds);
-        featureLayer.queryFeatures(query, function (error, featureCollection) {
-            if (error) {
-                console.error('Error querying features:', error);
-                return;
-            }
-            featureLayer.clearLayers();
-            featureLayer.addData(featureCollection.features);
+        console.log("WMS layer added to map:", currentWMSLayer);
+        updateFAB('Algforvaltningsomrade', true); // Säkerställ att FAB-knappen visas
+    } else {
+        if (currentWMSLayer) {
+            console.log('Removing Algforvaltningsomrade layer.');
+            map.off('click', wmsClickHandler);
+            map.removeLayer(currentWMSLayer);
+            currentWMSLayer = null;
+            wmsClickHandler = null;
+            hideZoomMessage(); // Dölj meddelandet när lagret tas bort
+            map.off('zoomend', checkZoomLevel); // Ta bort händelsen för zoomnivån
+            updateFAB('Algforvaltningsomrade', false); // Säkerställ att FAB-knappen döljs
+        }
+    }
+}
+
+function getFeatureInfoUrl(latlng, wmsLayer, map, params) {
+    var point = map.latLngToContainerPoint(latlng, map.getZoom());
+    var size = map.getSize();
+    var bounds = map.getBounds();
+    var crs = map.options.crs;
+
+    var sw = crs.project(bounds.getSouthWest());
+    var ne = crs.project(bounds.getNorthEast());
+
+    var defaultParams = {
+        request: 'GetFeatureInfo',
+        service: 'WMS',
+        srs: crs.code,
+        styles: '',
+        version: wmsLayer._wmsVersion,
+        format: wmsLayer.options.format,
+        bbox: sw.x + ',' + sw.y + ',' + ne.x + ',' + ne.y,
+        height: size.y,
+        width: size.x,
+        layers: wmsLayer.wmsParams.layers,
+        query_layers: wmsLayer.wmsParams.layers,
+        info_format: 'application/json'
+    };
+
+    params = L.Util.extend(defaultParams, params);
+
+    params[params.version === '1.3.0' ? 'i' : 'x'] = Math.round(point.x);
+    params[params.version === '1.3.0' ? 'j' : 'y'] = Math.round(point.y);
+
+    return wmsLayer._url + L.Util.getParamString(params, wmsLayer._url, true);
+}
+
+function handleWMSClick(e) {
+    if (currentWMSLayer) {
+        var latlng = e.latlng;
+        var url = getFeatureInfoUrl(latlng, currentWMSLayer, map, {
+            'info_format': 'text/xml',
+            'propertyName': 'Områdesnamn,Områdesnummer'
         });
+
+        console.log("GetFeatureInfo URL:", url);
+
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                console.log("FeatureInfo data:", data);
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(data, "application/xml");
+                var fields = xmlDoc.getElementsByTagName("FIELDS")[0];
+                if (fields) {
+                    var properties = {};
+                    for (var i = 0; i < fields.attributes.length; i++) {
+                        var attr = fields.attributes[i];
+                        properties[attr.name] = attr.value;
+                    }
+                    if (!popupPanelVisible) {
+                        showPopupPanel(properties);
+                    } else {
+                        updatePopupPanelContent(properties);
+                    }
+                } else {
+                    console.log("No features found.");
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching feature info:", error);
+            });
     }
-
-    // Uppdatera datalagret när användaren panorerar eller zoomar
-    window.map.on('moveend', updateFeatureLayer);
-
-    // Initial uppdatering när kartan först laddas
-    updateFeatureLayer();
 }
