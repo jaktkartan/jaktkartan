dd86a0cd930f4846dae32366383cc731d872befa
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-08-04 21:50:51 +0200

    Update popupHandler.js

--- popupHandler.js
+++ popupHandler.js
@@ -30,7 +30,7 @@ styleTag.innerHTML = `
     }
 
     #popup-panel p {
-        margin: 0; /* Ta bort marginaler från p-element */
+        margin: 3; /* Ta bort marginaler från p-element */
     }
 
     #close-button {
